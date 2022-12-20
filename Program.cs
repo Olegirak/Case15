aabbf71cc13f27e5dc8d4e6373ff87e85a771239
Oleg Iraklionov <irakliov@mail.ru>
2022-12-20 20:13:45 +0300

    Case15

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿// 
+﻿// Программа принимает на вход цифру дня недели и проверяет выходной
+int N = int.Parse(Console.ReadLine());
+if (N<1 || N>7)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{
+    if (N == 6 || N ==7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
